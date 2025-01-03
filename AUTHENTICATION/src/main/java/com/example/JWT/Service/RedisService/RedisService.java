476b7265325aea8227d46ca6e49e7e72f66a44ed
Dhruv Patel <Dhruv.21patel01@gmail.com>
2025-01-03 00:50:06 -0800

    removes independent Auth Service

--- AUTHENTICATION/src/main/java/com/example/JWT/Service/RedisService/RedisService.java
+++
@@ -1,8 +0,0 @@
-package com.example.JWT.Service.RedisService;
-
-public interface RedisService {
-
-    public <T> T get(String username, Class<T> entityclass);
-    public void set(String username, Object o);
-
-}
