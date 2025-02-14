0f5e58422a20e8a1e63e737c98fc64e1feec80f5
Dhruv Patel <Dhruv.21patel01@gmail.com>
2025-02-13 19:36:14 -0800

    few changes to proto files

--- Talkative/IndividualService/src/main/java/org/example/IndividualService/Services/grpcService.java
+++ Talkative/IndividualService/src/main/java/org/example/IndividualService/Services/grpcService.java
@@ -1,9 +1,16 @@
 package org.example.IndividualService.Services;
 
+import io.grpc.stub.StreamObserver;
+import org.example.IndividualService.IndividualMessageServiceGrpc;
 import org.example.IndividualService.MessageRequest;
+import org.example.IndividualService.ResponseMessage;
 import org.springframework.stereotype.Service;
 
 @Service
-public class grpcService {
+public class grpcService extends IndividualMessageServiceGrpc.IndividualMessageServiceImplBase {
 
+    @Override
+    public void getMessages(MessageRequest request, StreamObserver<ResponseMessage> responseObserver) {
+
+    }
 }
